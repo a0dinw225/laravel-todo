4141ec720ef3935018b8b7d49495b0343b6d58f7
a0dinw225 <93024617+a0dinw225@users.noreply.github.com>
2023-08-05 21:07:51 +0900

    Todoモデル、TodoDetailモデル、各FactoryとSeederを作成 (#2)
    Co-authored-by: a0dinw225 <hiro@MacBook-Pro>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Todo;
+use App\Models\TodoDetail;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Todo::factory(3)->create(['user_id' => $user->id])
+            ->each(function ($todo)  use ($user) {
+                TodoDetail::factory(5)->create([
+                    'user_id' => $user->id,
+                    'todo_id' => $todo->id
+                ]);
+            });
     }
 }
